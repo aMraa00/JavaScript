026188fe1e90a9709101607278a08c3aad9c001e
Your Name <abatzorig536@gmail.com>
2022-09-30 04:55:44 +0700

    first commit

---
+++ JavaScript/lesson07/script/app.js
@@ -0,0 +1,23 @@
+// Хичээл №7
+// Үндсэн операторуудын жишээ
+
+var year, yearJohn, yearMark;
+
+year = 2020;
+ageJonh = 20;
+ageMark = 45;
+
+console.log("Энэ жил бол : " + year);
+
+yearJohn = year - ageJonh;
+yearMark = year - ageMark;
+
+console.log("Жоны төрсөн он бол " + yearJohn);
+console.log("Маркын төрсөн он бол " + yearMark);
+
+// 15 жилийн дараа хэдэн он болох вэ?
+var future = year + 15;
+console.log(year + " оноос хойш 15 дараа " + future + " он байх болно...");
+//18 жилийн дараа Марк хэдэн настай байх вэ?
+var ageMarkAfter18Year = ageMark + 18;
+console.log("18 жилийн дараа Маркийн нас : ", ageMarkAfter18Year);
