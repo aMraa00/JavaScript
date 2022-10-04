53ee3dcd1740ac897588a64836e0e101ced23aee
Your Name <abatzorig536@gmail.com>
2022-10-04 18:44:39 +0700

    zaswar

---
+++ JavaScript/lesson10/script/app.js
@@ -0,0 +1,15 @@
+// 95<A,    95>A->89, 89>B+>87, 87>B>84, 84>C>75, 74>D>65, 65>F>50
+var dun = 00;
+var hahuulMongo = 0;
+var hamaatan = true;
+var nadadTaalagddag = true;
+
+if (
+  dun > 95 ||
+  hahuulMongo > 500000 ||
+  (hamaatan === true && nadadTaalagddag === true)
+) {
+  console.log("Та А авлаа");
+} else {
+  console.log("Уучлаарай танд А тавьж чадахгүй");
+}
