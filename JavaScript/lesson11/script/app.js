77b2a114ed2c79e76e04dd541b633f07a29e1e66
Your Name <abatzorig536@gmail.com>
2022-10-04 19:27:32 +0700

    lesson 11 nemsen

---
+++ JavaScript/lesson11/script/app.js
@@ -0,0 +1,5 @@
+var name = "Саранцэцэг";
+var age = 20;
+var who = age >= 18 ? "Том хүн" : "Хүүхэд";
+
+who === "Том хүн" ? console.log(name + " бол баранд орж болно") : console.log(name + " бол баранд орж болохгүй");
