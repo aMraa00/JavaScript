5df73147f5d708e1230694a14fe5af97ad4d873a
Your Name <abatzorig536@gmail.com>
2022-10-10 18:21:56 +0700

    lesson 15 nemsen

--- JavaScript/lesson15/script/app.js
+++ JavaScript/lesson15/script/app.js
@@ -1,3 +1,15 @@
-var module = function(number){
-    
+var module = function(too){
+    var mod;
+    if(too < 0) mod = -too
+    else mod = too;
+
+    return mod;
 }
+
+var addModules = function(x, y, z){
+    var niilber = module(x) + module(y) + module(z);
+    return niilber;
+}
+
+var sum = addModules(56, -10, -20);
+console.log(sum);
